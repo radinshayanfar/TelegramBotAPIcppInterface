1e88514a23bb72db672f3089d5b35c6ef9bf70df
radinshayanfar <radin.shayanfar@gmail.com>
2015-09-22 15:12:08 +0330

    Update Methods.cpp
    Fixed problem with multiple definition...

--- cppTelegramBotAPIInterface/Methods.cpp
+++ cppTelegramBotAPIInterface/Methods.cpp
@@ -7,6 +7,13 @@ using namespace libCURL;
 
 namespace Methods
 {
+	const string BOTAPI = "https://api.telegram.org/botTOKEN/";
+	
+	struct curl_httppost *formpost=NULL;
+	struct curl_httppost *lastptr=NULL;
+	struct curl_slist *headerlist=NULL;
+	static const char buf[] = "Expect:";
+	
 	void getMe()
 	{
 		string URL = BOTAPI + "getMe";
@@ -16,12 +23,12 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "getMe.json");
+		CURL *curl = init(c, "..//getMe.json");
 		
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendMessage(int chat_id, string text, bool disable_web_page_preview = false, int reply_to_message_id = 0, string reply_markup = "")
+	void sendMessage(int chat_id, string text, bool disable_web_page_preview, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendMessage";
 
@@ -30,7 +37,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendMessage.json");
+		CURL *curl = init(c, "..//sendMessage.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "text", CURLFORM_COPYCONTENTS, text.c_str(), CURLFORM_END);
@@ -50,7 +57,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "forwardMessage.json");
+		CURL *curl = init(c, "..//forwardMessage.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "from_chat_id", CURLFORM_COPYCONTENTS, to_string(from_chat_id).c_str(), CURLFORM_END);
@@ -59,7 +66,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendPhoto(int chat_id, string photo, string caption = "", int reply_to_message_id = 0, string reply_markup = "")
+	void sendPhoto(int chat_id, string photo, string caption, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendPhoto";
 		// ============ convert string to char array =========== // 
@@ -67,7 +74,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendPhoto.json");
+		CURL *curl = init(c, "..//sendPhoto.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "photo", CURLFORM_FILE, photo.c_str(), CURLFORM_END);
@@ -78,7 +85,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendAudio(int chat_id, string audio, int duration = 0, string performer = "", string title = "", int reply_to_message_id = 0, string reply_markup = "")
+	void sendAudio(int chat_id, string audio, int duration, string performer, string title, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendAudio";
 		// ============ convert string to char array =========== // 
@@ -86,7 +93,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendAudio.json");
+		CURL *curl = init(c, "..//sendAudio.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "audio", CURLFORM_FILE, audio.c_str(), CURLFORM_END);
@@ -99,7 +106,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendDocument(int chat_id, string document, int reply_to_message_id = 0, string reply_markup = "")
+	void sendDocument(int chat_id, string document, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendDocument";
 		// ============ convert string to char array =========== // 
@@ -107,7 +114,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendDocument.json");
+		CURL *curl = init(c, "..//sendDocument.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "document", CURLFORM_FILE, document.c_str(), CURLFORM_END);
@@ -117,7 +124,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendSticker(int chat_id, string sticker, int reply_to_message_id = 0, string reply_markup = "")
+	void sendSticker(int chat_id, string sticker, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendSticker";
 		// ============ convert string to char array =========== // 
@@ -125,7 +132,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendSticker.json");
+		CURL *curl = init(c, "..//sendSticker.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "sticker", CURLFORM_FILE, sticker.c_str(), CURLFORM_END);
@@ -135,7 +142,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendVideo(int chat_id, string video, int duration = 0, string caption = "", int reply_to_message_id = 0, string reply_markup = "")
+	void sendVideo(int chat_id, string video, int duration, string caption, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendVideo";
 		// ============ convert string to char array =========== // 
@@ -143,7 +150,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendVideo.json");
+		CURL *curl = init(c, "..//sendVideo.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "video", CURLFORM_FILE, video.c_str(), CURLFORM_END);
@@ -155,7 +162,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendVoice(int chat_id, string voice, int duration = 0, int reply_to_message_id = 0, string reply_markup = "")
+	void sendVoice(int chat_id, string voice, int duration, int reply_to_message_id, string reply_markup)	
 	{
 		string URL = BOTAPI + "sendVoice";
 		// ============ convert string to char array =========== // 
@@ -163,7 +170,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendVoice.json");
+		CURL *curl = init(c, "..//sendVoice.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "voice", CURLFORM_FILE, voice.c_str(), CURLFORM_END);
@@ -174,7 +181,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void sendLocation(int chat_id, float latitude, float longitude, int reply_to_message_id = 0, string reply_markup = "")
+	void sendLocation(int chat_id, float latitude, float longitude, int reply_to_message_id, string reply_markup)
 	{
 		string URL = BOTAPI + "sendLocation";
 		// ============ convert string to char array =========== // 
@@ -182,7 +189,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendLocation.json");
+		CURL *curl = init(c, "..//sendLocation.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "latitude", CURLFORM_COPYCONTENTS, to_string(latitude).c_str(), CURLFORM_END);
@@ -201,7 +208,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "sendChatAction.json");
+		CURL *curl = init(c, "..//sendChatAction.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "chat_id", CURLFORM_COPYCONTENTS, to_string(chat_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "action", CURLFORM_COPYCONTENTS, action.c_str(), CURLFORM_END);
@@ -209,7 +216,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void getUserProfilePhotos(int user_id, int offset = 0, int limit = 100)
+	void getUserProfilePhotos(int user_id, int offset, int limit)
 	{
 		string URL = BOTAPI + "getUserProfilePhotos";
 		// ============ convert string to char array =========== // 
@@ -217,7 +224,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "getUserProfilePhotos.json");
+		CURL *curl = init(c, "..//getUserProfilePhotos.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "user_id", CURLFORM_COPYCONTENTS, to_string(user_id).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "offset", CURLFORM_COPYCONTENTS, to_string(offset).c_str(), CURLFORM_END);
@@ -226,7 +233,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void getUpdates(int offset = 0, int limit = 100, int timeout = 0)
+	void getUpdates(int offset, int limit, int timeout)
 	{
 		string URL = BOTAPI + "getUpdates";
 		// ============ convert string to char array =========== // 
@@ -234,7 +241,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "getUpdates.json");
+		CURL *curl = init(c, "..//getUpdates.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "offset", CURLFORM_COPYCONTENTS, to_string(offset).c_str(), CURLFORM_END);
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "limit", CURLFORM_COPYCONTENTS, to_string(limit).c_str(), CURLFORM_END);
@@ -243,7 +250,7 @@ namespace Methods
 		curl_easy_perform(curl);
 	    curl_easy_cleanup(curl);
 	}
-	void setWebhook(string url = "", string certificate = "")
+	void setWebhook(string url, string certificate)
 	{
 		string URL = BOTAPI + "setWebhook";
 		// ============ convert string to char array =========== // 
@@ -251,7 +258,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "setWebhook.json");
+		CURL *curl = init(c, "..//setWebhook.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "url", CURLFORM_COPYCONTENTS, url.c_str(), CURLFORM_END);
 		if (certificate != "")
@@ -268,7 +275,7 @@ namespace Methods
 		strncpy(c, URL.c_str(), sizeof(c));
 		c[sizeof(c) - 1] = 0;
 		
-		CURL *curl = init(c, "getFile.json");
+		CURL *curl = init(c, "..//getFile.json");
 		// ============ add fields =========== // 
 		curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "file_id", CURLFORM_COPYCONTENTS, file_id.c_str(), CURLFORM_END);
 		curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
